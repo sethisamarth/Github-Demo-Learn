ea0e51b7f6c582f96225efcb14058769978cd780
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-28 10:24:20 +0530

    emp attendence

---
+++ GitHub/GitHub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int is_present = 1;
+            Random random = new Random();
+
+            int empcheck = random.Next(0, 2);
+            if (empcheck == is_present)
+            {
+                Console.WriteLine("employ is present");
+            }
+            else
+            {
+                Console.WriteLine("employ is absent");
+            }
+        }
+    }
+}
