f66e8c2ffe2ef58be144ac5deccf71427ddfc640
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-28 10:33:44 +0530

    Emp wage for fix hr

--- GitHub/GitHub/Program.cs
+++ GitHub/GitHub/Program.cs
@@ -8,15 +8,20 @@ namespace GitHub
         {
             int emp_rate_per_hr = 20;
             int is_present = 1;
+            int is_part_present = 2;
             int emp_hr = 0;
             int emp_wage = 0;
 
             Random random = new Random();
 
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if (empcheck == is_present)
             {
                 emp_hr = 8;
+            }
+            else if (empcheck == is_part_present)
+            {
+                emp_hr = 4;
             }
             else
             {
